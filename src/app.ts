e2969fa00dd995771d08c9c9e049bed43a31746a
Rafael Kammer <rafa.br00@gmail.com>
2022-04-09 14:24:06 -0300

    Cors correction, to run back and front at the same time locally

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { json } from "express";
 import router from "./routes";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(json());
+app.use(cors());
 app.use("", router);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.RUN_PORT ? process.env.RUN_PORT : 3000;
+const PORT = process.env.RUN_PORT ? process.env.RUN_PORT : 3001;
 
 const server = app.listen(PORT, () =>
   console.log(`App is running at: http://localhost:${PORT}`)
